8a57217e92beb2a0131cbfae87cbc62fd4ce6af5
Danilo Arroteia <danilo.arroteia@compasso.com.br>
2023-06-06 21:44:44 -0300

    feat: home page

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import { GlobalStyle } from "./styles/global";
 import { lightTheme, darkTheme } from "./styles/themes/theme";
-import { Router } from "./components/Router";
+import { Router } from "./routes";
 
 export function App() {
   const [theme, setTheme] = useState(lightTheme);
