b4f0a287c9d3b118aeada91787aa93b8f4db7c21
Danilo Arroteia <danilo.arroteia@compasso.com.br>
2023-06-07 11:41:27 -0300

    feat: react-hook-form

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Play } from "phosphor-react";
 import {
   CountdownContainer,
@@ -10,15 +12,27 @@ import {
 } from "./styles";
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm();
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+  // Verificar se task está preenchido
+  const task = watch("task");
+  // Define se o botão de submit está habilitado ou não
+
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -33,8 +47,9 @@ export function Home() {
             id="minutesAmount"
             placeholder="00"
             step={5}
-            min={1}
+            min={0}
             max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -48,7 +63,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
